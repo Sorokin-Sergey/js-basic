9fdddd9172a36acb082905fcaf25ca107270ca19
SorokinSergei <sorokin_ser@mail.ru>
2023-07-06 20:38:47 +0400

    Exercise 2

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
 /*
-    Ваша часовая ставка 80$ и вы готовы работать не более 5 часов в день 5 дней в неделю (кроме выходных).
-    К вам приходит заказчик и предлагает заказ на 40 часов работы. Сейчас понедельник. Вы должны уехать через 11 дней.
-    Выведете в консоль:
-    Boolean переменную успеете ли вы взяться за работу
-    Сколько вы за неё попросите?
+    Васи положил 12 000$ на вклад 7% годовых с капитализацией 1 раз в месяц. Вывести в консоль, сможет ли он купить дом за 13 500$ через 2 года после снятия вклада. И остаток после покупки.
+    Итог = Сумма * (1 + Ставка в месяц не в %) ^ срок в месяцах
 */
 
-const payRateUsd = 80;
-const hoursPerDay = 5;
-const daysInWeek = 5;
-const daysToHolidays = 11;
+const months = 24;
+const deposit = 12000;
+const rate = 7;
+const needsMoney = 13500;
 
+const resultSumm = deposit * (1 + rate / 100 / 12) ** months;
 
-function getJob (hoursForProject) {
-    let needDays = Math.ceil(hoursForProject/hoursPerDay);
-    const payment = hoursForProject*payRateUsd;
-    const needWeeks = Math.floor(needDays/5);
-    needDays += needWeeks * 2;
-    const success = needDays <= daysToHolidays;
-    return {
-        needDays,
-        payment,
-        success
-    }
+let text = '';
+
+if (resultSumm > needsMoney) {
+    text = `Денег на покупку дома хватит. Остаток составляет ${resultSumm - needsMoney}`;
+} else {
+    text = `Денег не хватит на покупку дома. Не хватает ${needsMoney - resultSumm}`;
 }
 
-let result = getJob(40);
-
-console.log(result.needDays);
-console.log(result.payment);
-console.log(result.success);
+console.log(text);
