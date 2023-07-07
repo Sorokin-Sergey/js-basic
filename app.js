7b4fd217166f98166691379b3b52419b0d3aeba6
SorokinSergei <sorokin_ser@mail.ru>
2023-07-07 23:52:40 +0400

    Exercise 8

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
-/* 
-    Задача вывести в консоль строку "Я люблю JS !" из массива,
-    проходя циклом в обратном порядке, не используя метод reverse.
-    const arr = ['!', 'JS', 'люблю', 'Я'];
+/*
+    Есть выгрузка операций пользователя
+    const operations = [1000, -700, 300, -500, 10000];
+    а так же начальный баланс в 100$
+    Необходимо сделать функции расчета:
+    - Итогового баланса
+    - Наличия отрицательного баланса (если после очередной операции баланс < 0, то выдавать false)
+    - Расчета среднего расхода и среднего дохода
 */
 
-const arr = ['!', 'JS', 'люблю', 'Я'];
-const newArr = [];
+const operations = [1000, -700, 300, -500, 10000];
+const startBalance = 100;
 
-for (let i = arr.length-1; i >= 0; i--) {
-    newArr.push(arr[i]);
+function avg (operations) {
+    let positiveAvg = 0;
+    let cntPositive = 0;
+    let negativeAvg = 0;
+    let cntNegative = 0;
+    for (const operation of operations) {
+        if (operation > 0) {
+            positiveAvg += operation;
+            cntPositive++;
+        } else if (operation < 0) {
+            negativeAvg += operation;
+            cntNegative++;
+        }
+    }
+    console.log(`Средний доход ${positiveAvg / cntPositive}`);
+    console.log(`Средний расход ${negativeAvg / cntNegative}`);
 }
 
-console.log(newArr.join(' '));
+function resultBalance (operations, startBalance) {
+    let balance = startBalance;
+    for (const operation of operations) {
+        balance += operation;
+        if (balance < 0) {
+            return false;
+        }
+    }
+    return balance;
+}
+
+console.log(resultBalance(operations, startBalance));
+
+avg(operations);
