b3debd0218976e99cd21331814deee0de9e2993f
SorokinSergei <sorokin_ser@mail.ru>
2023-07-07 12:17:28 +0400

    Exercise 4

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 /*
-    Методом prompt получите ответ пользователя на вопрос "Сколько будет 7 + или - 15?". Если ответ верен, выведите в консоли "Успех", если нет - "Вы робот!", а если он введёт "Я не робот", то тоже "Успех".
+    Задание для упражнения:
+    Пользователь:
+    Возраст
+    Наличие работы
+    Деньги
+    Нужно проверить может ли он купить новый MacBook за 2000$? Он может брать не только свои деньги, но и взять кредит. Ему дадут 500$, только если ему больше 24-х лет и он имеет работу, 100$ если ему просто больше 24-х лет и 0 в ином случае. Напишите функцию, которая принимает данные пользователя и товара и возвращает true или false.
 */
 
-const result = prompt("Сколько будет 7 + или - 15?");
-let isRobot;
+class User {
+    constructor (name, age, hasWork, money) {
+        this.name = name;
+        this.age = age;
+        this.hasWork = hasWork;
+        this.money = money;
+    }
+    
+    getCredit = () => {
+        if (this.age > 24) {
+            return this.hasWork ? 500 : 100;
+        } else {
+            return 0;
+        }
+    }
 
-switch(true) {
-    case result === "Я не робот":
-    case Number(result) === 22:
-    case Number(result) === -8:
-        isRobot = false;
-        break;
-    default:
-        isRobot = true;
+    canBuy = costLaptop => (this.money + this.getCredit()) >= costLaptop
 }
 
-console.log(isRobot ? 'Вы робот!' : 'Успех!');
+const vasya = new User('Вася', 25, true, 1600);
+
+console.log(vasya.canBuy(2000));
