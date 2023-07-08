19bb6d46e936eae3fb4bd5c25ccc23f49f2ec2e5
SorokinSergei <sorokin_ser@mail.ru>
2023-07-08 14:32:34 +0400

    Exercise 10

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-// Написать функцию возведения в степень с использованием 2 стрелочных функций
+/*
+Найти среднее значение последоватедьности
+чисел с помощью reduce
+*/
 
-const power = power => num => num**power;
+const arr = [2, 4, 4, 10];
 
-console.log(power(3)(5));
+const avg = arr.reduce((acc, val, i) => {
+    if (i < arr.length -1) {
+        return acc + val
+    } else {
+        return (acc + val) / arr.length;
+    }
+}, 0);
+
+console.log(avg);
