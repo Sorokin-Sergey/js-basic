4e363eec5f9914e7a2fb5e7e393a16f54aed695e
SorokinSergei <sorokin_ser@mail.ru>
2023-07-09 19:27:48 +0400

    Exercise 11

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-/*
-Найти среднее значение последоватедьности
-чисел с помощью reduce
-*/
+//Задание: проверить является ли это номером телефона России
+/* верные */
+const num1 = '89103235356';
+const num2 = '+79103235356';
+const num3 = '+7(910)3235356';
+const num4 = '+7(910) 323-53-56';
+const num5 = ' +7(910) 323-53-56 ';
+/* не верные */
+const num1Error = '89103235';
+const num2Error = '+7d910d323-53-56';
+const num3Error = '9+7103235356';
+const num4Error = '89103g35356';
 
-const arr = [2, 4, 4, 10];
+function isPhoneNumber(phone) {
+    phone = phone
+                .trim()
+                .replace('+7', '8')
+                .replace('(', '')
+                .replace(')', '')
+                .replace(/-/g, '')
+                .replace(/ /g, '');
+    return (phone.length != 11 || !phone.startsWith('8') || isNaN(phone)) ? false : true;
+}
 
-const avg = arr.reduce((acc, val, i) => {
-    if (i < arr.length -1) {
-        return acc + val
-    } else {
-        return (acc + val) / arr.length;
-    }
-}, 0);
-
-console.log(avg);
+console.log(isPhoneNumber(num1));
+console.log(isPhoneNumber(num2));
+console.log(isPhoneNumber(num3));
+console.log(isPhoneNumber(num4));
+console.log(isPhoneNumber(num5));
+console.log(isPhoneNumber(num1Error));
+console.log(isPhoneNumber(num2Error));
+console.log(isPhoneNumber(num3Error));
+console.log(isPhoneNumber(num4Error));
